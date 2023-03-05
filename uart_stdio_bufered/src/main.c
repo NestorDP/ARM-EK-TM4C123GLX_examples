ed7bd952fd89972a1bec75e13f1c7925a71f1159
NestorDP <nestor-dp@hotmail.com>
2023-03-04 21:21:54 -0300

    uart burfered done

---
+++ uart_stdio_bufered/src/main.c
@@ -0,0 +1,81 @@
+#include <stdint.h>                         
+#include <stdbool.h>                        
+#include "inc/hw_memmap.h"                  
+#include "inc/hw_types.h"                   
+#include "inc/hw_gpio.h"                    
+#include "inc/hw_qei.h"                     
+#include "driverlib/debug.h"                
+#include "driverlib/sysctl.h"               
+#include "driverlib/interrupt.h"            
+#include "driverlib/gpio.h"                 
+#include "driverlib/qei.h"                  
+#include "driverlib/pin_map.h"              
+#include "driverlib/rom.h"                  
+
+#include "stdlib.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_timer.h"
+#include "inc/hw_uart.h"
+
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "driverlib/pwm.h"
+
+#include "utils/uartstdio.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+
+#include "utils/uartstdio.h"
+
+
+void UARTStdioIntHandler (void);
+
+int main(void){
+    // Set the System clock to 80MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5 |SYSCTL_USE_PLL |SYSCTL_OSC_MAIN |SYSCTL_XTAL_16MHZ);
+	
+    // Enable GPIO port A which is used for UART0 pins.
+    // TODO: change this to whichever GPIO port you are using.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlDelay(3);
+    
+    // Configure the pin muxing for UART0 functions on port A0 and A1.
+    // This step is not necessary if your part does not support pin muxing.
+    // TODO: change this to select the port/pin you are using.
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    
+    // Enable UART0 so that we can configure the clock.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    
+    // Use the internal 16MHz oscillator as the UART clock source.
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    
+    // Select the alternate (UART) function for these pins.
+    // TODO: change this to select the port/pin you are using.
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(0, 115200, 16000000);
+
+        // Master interrupt enable API for all interrupts
+    IntMasterEnable();
+	
+    while (1){
+		UARTprintf("\nvelocidade: ");
+		SysCtlDelay( 500 * (80000000 / 3 / 1000));
+	}
+}
